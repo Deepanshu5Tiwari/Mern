2a52fb369cb1f62ce7fc6fe2ee172b03ff8629e6
Deepanshu5tiwari <tdeepanshu00@gmail.com>
2024-07-31 22:05:36 +0530

    react3

--- section 2/frontend/src/app/login/page.jsx
+++ section 2/frontend/src/app/login/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import classes from './login.module.css';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+
 
 const Login = () => {
   return (
     <div>
       <h1 className='text-center text-2xl font-bold mt-6 underline text-blue-500 '>Login Page</h1>
+      <button className='btn '>Login Button</button>
+      <button className={classes.myBtn}>Module CSS</button>
+
+      <Input id={'name'} label={'Full Name'} type={'text'}/> 
+      <Input id={'email'} label={'Email Address'} type={'email'}/>
+      <Input id={'password'} label={'Your Password'} type={'password'}/>
+
+      <Button>Login</Button>
+      
+
     </div>
   )
 }
