2f276c09f08d3485884a64fe068b75d6416c7cfe
Deepanshu5tiwari <tdeepanshu00@gmail.com>
2024-08-11 19:20:17 +0530

    mongoose

--- section 3/Backend/routers/productRouter.js
+++ section 3/Backend/routers/productRouter.js
@@ -4,4 +4,19 @@ const router = express.Router();
 router.get('/', (req, res) =>{res.send ('All Products are open now')})
 
 router.get('/item1', (req, res) => {res.send ('Item! not yet declared')})
+
+router.post('/delivery', (req, res) => {
+    console.log(req.body);
+    res.send('Yet To Delivered')
+    
+})
+
+
+router.get('/getbyid/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send('get it by id')
+    
+})
+
+
 module.exports = router;
