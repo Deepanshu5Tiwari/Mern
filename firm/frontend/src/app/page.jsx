23043b40716607f1a88d06e131246cff1ef247a3
Deepanshu5tiwari <tdeepanshu00@gmail.com>
2026-07-25 14:32:41 +0530

    eco friendly mard

---
+++ firm/frontend/src/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <div className='font-bold text-7xl '>
+      <h1 className='text-red-500'>HEELO JI  here</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default home;
